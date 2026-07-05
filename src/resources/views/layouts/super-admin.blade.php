a24a785f388cf868a887d73d04ec6fc357a97a00
Dapa102 <Dapa102@users.noreply.github.com>
2026-07-05 10:35:42 +0700

    fix: bust favicon cache, add v param

--- src/resources/views/layouts/super-admin.blade.php
+++ src/resources/views/layouts/super-admin.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="icon" href="{{ asset('images/logo.svg') }}" type="image/svg+xml">
+    <link rel="icon" href="{{ asset('images/logo.svg?v=2') }}" type="image/svg+xml">
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
