4b862d920f778271d6431e57f26138c9f54a8fe6
Dapa102 <Dapa102@users.noreply.github.com>
2026-07-05 12:45:36 +0700

    fix: use $wire.showDetail via Alpine for all legend buttons

--- src/app/Livewire/Pm/PmDashboard.php
+++ src/app/Livewire/Pm/PmDashboard.php
@@ -8,7 +8,6 @@ use App\Models\User;
 use App\Models\Task;
 use App\Services\TaskStatusHistoryService;
 use Livewire\Attributes\Layout;
-use Livewire\Attributes\On;
 use Carbon\Carbon;
 
 #[Layout('layouts.pm')]
@@ -98,7 +97,6 @@ class PmDashboard extends Component
         $this->reset(['reviewNote', 'rejectTaskId']);
     }
 
-    #[On('showDetail')]
     public function showDetail($label)
     {
         $this->detailTitle = $label;
